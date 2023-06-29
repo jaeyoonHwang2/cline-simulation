a1f2e6df7b6f383b7cacb322bc7c857688e55b19
nxcl <jaeyoon@snu.ac.kr>
2023-06-29 21:25:09 +0900

    first commit

---
+++ build/build-status.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python3
+
+# Programs that are runnable.
+ns3_runnable_programs = ['build/scratch/train_cat/ns3.35-train_cat-debug', 'build/scratch/cc_230327/ns3.35-cc_230327-debug', 'build/scratch/revise_cc/ns3.35-revise_cc-debug', 'build/scratch/infer_cat/ns3.35-infer_cat-debug', 'build/scratch/subdir/ns3.35-subdir-debug', 'build/scratch/ns3.35-scratch-simulator-debug']
+
+# Scripts that are runnable.
+ns3_runnable_scripts = []
+
